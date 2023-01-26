ea46729ac8094724956fc97d5485e9ef2f5c02f4
Jan Misiurek <jan.misiurek@gmail.com>
2023-01-26 22:31:28 +0100

    added view of seleced table

--- website/views.py
+++ website/views.py
@@ -7,23 +7,40 @@ def home():
 
     if request.method == 'POST':
         from . import get_tables
-
+        
         url = request.form.get('url')
-        pageTables = get_tables(url)
-        tablesAmount = pageTables['n_tables']
-        pageTitle = pageTables['site']
-        tables = pageTables['tables']
-        tablesTitles = pageTables['titles']
-        tablesHTML = []
-        for table in tables:
-            tablesHTML.append(table.to_html())
+        if url is not None:
+
+            global pageTables 
+            pageTables = get_tables(url)
+            global tablesAmount
+            tablesAmount = pageTables['n_tables']
+            global pageTitle
+            pageTitle = pageTables['site']
+            global tables
+            tables = pageTables['tables']
+            global tablesTitles
+            tablesTitles = pageTables['titles']
+            global tablesHTML
+            tablesHTML = []
+            global tableView
+            tableView = ''
+            
+            for table in tables:
+                tablesHTML.append(table.to_html())
+
+
+        tableID = request.form.get('tableID')
+        if tableID is not None:
+            tableView = tablesHTML[int(tableID)]
 
         return render_template(
             "home.html",
             tablesAmount = tablesAmount,
             pageTitle = pageTitle,
             tables = tablesHTML,
-            tablesTitles = tablesTitles
+            tablesTitles = tablesTitles,
+            tableView = tableView
         )
     else:
         return render_template("home.html")
